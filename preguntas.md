96344f31a35350934851c8e1210851e07f2b9416
Andres Carrasquilla Git <andresjulian.carrasquilla@gmail.com>
2018-03-10 12:06:06 -0500

    Examen 1

--- preguntas.md
+++ preguntas.md
@@ -1,9 +1,29 @@
 # Preguntas (10% c/u)
 
 1. ¿Qué acciones ***no*** debería realizar una función pura?
+    Las funciones puras no deberian realizar lo siguiente:
+     - No deben tomar variables globales
+     - No deben llamar funciones que no sean puras
+     - No deben tener datos mutables
+     - Siempre deben retornar los mismos datos para la misma entrada
+
+
 2. ¿Qué se gana al crear funciones puras?
+    Las funciones puras no estan acopladas, por lo tanto es más sencillo realizar pruebas.
+    El codigo de estas es más entendible, y debido a que no necesitan datos externos se
+    pueden paralelizar de manera sencilla.
+    El negocio de un proyecto deberia ser con funciones puras, debido a que con estas,
+    se puede garantizar la integridad del codigo.
+
 3. ¿Por qué diseñar datos inmutables?
+    Es mejor diseñar con datos inmutables debido a que los datos mutables pueden cambiar en
+    cualquier momento de estado, por lo tanto al usar datos imutables garantizamos integridad
+    en los datos.
+
 4. ¿Por qué son importantes los tipos?
+    Los tipos son importantes para definir claramente con que estamos trabajando, esto nos
+    ayuda a filtrar la informacion que le puede llegar a un método, y así, encontrar más facil
+    errores en tiempo de complilación.
 
 # Tests Passed(4% c/u) 
 
--- src/main/scala/exam/cons.scala
+++ src/main/scala/exam/cons.scala
@@ -8,32 +8,61 @@ final case class Cons[+A](head: A, tail: Collection[A]) extends Collection[A]
 
 object Collection {
   def apply[A](as: A*): Collection[A] = as.toList match {
-    case cons if cons.isEmpty => ???
-    case _ :: t => Cons(???, apply(t: _*))
+    case cons if cons.isEmpty => Empty
+    case h :: t => Cons(h, apply(t: _*))
   }
 
   def foldRight[A, B](col: Collection[A], z: B)(f: (A, B) => B): B = col match {
-    case Empty => ???
-    case Cons(_, t) => f(???, foldRight(t, z)(???))
+    case Empty => z
+    case Cons(h, t) => f(h, foldRight(t, z)(f))
   }
 
-  def length(col: Collection): Int = foldRight(col, ???) { ??? }
+  def length[A](col: Collection[A]): Int = foldRight(col, Integer.MIN_VALUE) { ??? }
 }
 
 object CollectionInt {
   def sumFold(col: Collection[Int]): Int = Collection.foldRight(col, ???)(???)
 
-  def sumRec(col: Collection[Int]) = {
+  def sumRec(l: Collection[Int]): Int = {
     @tailrec
-    def sumAccumulator = ???
-    ???
+    def sumAccumulator(elm: Collection[Int], acum: Int): Int = {
+      elm match {
+        case Cons(h, Empty) => h + acum
+        case Cons(h, t) => sumAccumulator(t, (h + acum))
+        case Empty => acum
+      }
+    }
+    sumAccumulator(l, 0)
   }
 
   def productFold(col: Collection[Int]) = ???
 
-  def productRec(col: Collection[Int]) = ???
+  //def productRec(col: Collection[Int]) = ???
 
-  def maxFold(col: Collection[Int]) = Collection.foldRight(col, Integer.MIN_VALUE)(_ max _)
+  def productRec(l: Collection[Int]): Int = {
+    @tailrec
+    def proAccumulator(elm: Collection[Int], acum: Int): Int = {
+      elm match {
+        case Cons(h, Empty) => h * acum
+        case Cons(h, t) => proAccumulator(t, (h * acum))
+        case Empty => acum
+      }
+    }
+    proAccumulator(l, 1)
+  }
 
-  def maxRec(col: Collection[Int]): Int = ???
+  def maxFold(col: Collection[Int]) = Collection.foldRight(col, Integer.MAX_VALUE)(_ max _)
+
+  def maxRec(l: Collection[Int]): Int = {
+    @tailrec
+    def max(elm: Collection[Int], maximo: Int):
+    Int ={
+      elm match {
+        case Cons(h, Empty) => if (h >= maximo) h else maximo
+        case Cons(h, t) => max(t, if (h >= maximo) h else maximo)
+        case Empty => maximo
+      }
+    }
+    max(l, 0)
+  }
 }
